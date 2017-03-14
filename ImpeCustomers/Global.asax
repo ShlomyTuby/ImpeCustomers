712906dcc32de7c06de0b1f69b1698ee05ea4340
Shlomy Tuby <shlomy.tuby@gmail.com>
2017-03-14 20:38:43 +0200

    Add project files.

---
+++ ImpeCustomers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImpeCustomers.MvcApplication" Language="C#" %>
---
+++ ImpeCustomers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImpeCustomers.Startup))]
+namespace ImpeCustomers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
